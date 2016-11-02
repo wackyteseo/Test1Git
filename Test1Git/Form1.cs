481f41da5d45218f356ec030b4dd57d66371f90f
DESKTOP-NEM2CHI\walte <wackyteseo@live.it>
2016-11-02 13:24:29 +0100

    Primo

--- Test1Git/Form1.cs
+++ Test1Git/Form1.cs
@@ -16,5 +16,10 @@ namespace Test1Git
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test1");
+        }
     }
 }
